387801f521825d7404ff4ffd6cb97d5bf3020f55
KorneevAlexandr <sansay_sa@mail.ru>
2022-11-01 21:27:21 +0300

    Implement task1

--- ExceptionHandling/Task1/Program.cs
+++ ExceptionHandling/Task1/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            while (true)
+            {
+                Console.WriteLine("\nEnter the line: ");
+                var line = Console.ReadLine();
+
+                try
+                {
+                    Console.WriteLine(line[0]);
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine("Input string was null.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Something went wrong: {ex.Message}");
+                }
+            }
         }
     }
 }
