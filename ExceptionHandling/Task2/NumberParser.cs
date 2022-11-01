109d61d645877ae3a47aaf7994e3f4c27bfeb345
KorneevAlexandr <sansay_sa@mail.ru>
2022-11-01 22:12:04 +0300

    Implement task2

--- ExceptionHandling/Task2/NumberParser.cs
+++ ExceptionHandling/Task2/NumberParser.cs
@@ -1,12 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
 
 namespace Task2
 {
     public class NumberParser : INumberParser
     {
+        private const char Plus = '+';
+        private const char Minus = '-';
+
+        private readonly static Dictionary<char, int> ValidNumbers = new Dictionary<char, int>
+        {
+            { '0', 0 },
+            { '1', 1 },
+            { '2', 2 },
+            { '3', 3 },
+            { '4', 4 },
+            { '5', 5 },
+            { '6', 6 },
+            { '7', 7 },
+            { '8', 8 },
+            { '9', 9 }
+        };
+
         public int Parse(string stringValue)
         {
-            throw new NotImplementedException();
+            if (stringValue == null)
+            {
+                throw new ArgumentNullException(nameof(stringValue));
+            }
+
+            if (string.IsNullOrWhiteSpace(stringValue))
+            {
+                throw new FormatException("Input value is empty.");
+            }
+
+            var isMinus = false;
+            long numericValue = 0;
+
+            for (int i = 0; i < stringValue.Trim().Length; i++)
+            {
+                var symbol = stringValue[i];
+
+                if (i == 0 && symbol.Equals(Minus))
+                {
+                    isMinus = true;
+                    continue;
+                }
+                
+                if (i == 0 && symbol.Equals(Plus))
+                {
+                    continue;
+                }
+
+                var isValidNumber = ValidNumbers.TryGetValue(symbol, out int value);
+
+                if (!isValidNumber)
+                {
+                    throw new FormatException("Input value is not correct.");
+                }
+
+                numericValue = numericValue * 10 + value;
+            }
+
+            numericValue *= isMinus ? -1 : 1;
+            
+            if (numericValue > int.MaxValue || numericValue < int.MinValue)
+            {
+                throw new OverflowException("Input value less or big than possible.");
+            }
+
+            return (int)numericValue;
         }
     }
 }
