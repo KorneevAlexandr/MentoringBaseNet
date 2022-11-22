38f411613f97c8ab4a5f27300eaf5b39fef13c72
KorneevAlexandr <sansay_sa@mail.ru>
2022-11-22 22:32:08 +0300

    Implement DoublyLinkedList

--- DataStructures_6/Tasks/DoublyLinkedList.cs
+++ DataStructures_6/Tasks/DoublyLinkedList.cs
@@ -7,41 +7,197 @@ namespace Tasks
 {
     public class DoublyLinkedList<T> : IDoublyLinkedList<T>
     {
-        public int Length => throw new NotImplementedException();
+        private Node _firstNode;
+        private Node _lastNode;
+        private int _length;
+
+        public int Length => _length;
 
         public void Add(T e)
         {
-            throw new NotImplementedException();
+            if (_firstNode == null)
+            {
+                _firstNode = new Node(e, null, null);
+                _lastNode = _firstNode;
+            }
+            else
+            {
+                _lastNode.Next = new Node(e, _lastNode, null);
+                _lastNode = _lastNode.Next;
+            }
+
+            _length++;
         }
 
         public void AddAt(int index, T e)
         {
-            throw new NotImplementedException();
+            if (index == _length)
+            {
+                Add(e);
+            }
+            else
+            {
+                var currentNode = NodeAt(index);
+                var previousNode = currentNode.Previous;
+                var newNode = new Node(e, previousNode, currentNode);
+                currentNode.Previous = newNode;
+                _length++;
+                
+                if (previousNode != null)
+                {
+                    previousNode.Next = newNode;
+                }
+                else
+                {
+                    _firstNode = newNode;
+                }
+            }
         }
 
         public T ElementAt(int index)
         {
-            throw new NotImplementedException();
+            return NodeAt(index).Value;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new Enumerator(_firstNode);
         }
 
         public void Remove(T item)
         {
-            throw new NotImplementedException();
+            var node = _firstNode;
+
+            for (int i = 0; i < _length; i++)
+            {
+                if (node.Value.Equals(item))
+                {
+                    RemoveNode(node);
+                    _length--;
+
+                    break;
+                }
+
+                node = node.Next;
+            }
         }
 
         public T RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            var currentNode = NodeAt(index);
+            RemoveNode(currentNode);
+            _length--;
+
+            return currentNode.Value;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new Enumerator(_firstNode);
+        }
+
+        private Node NodeAt(int index)
+        {
+            if (index < 0 || index >= _length)
+            {
+                throw new IndexOutOfRangeException("Specified index was out of range.");
+            }
+
+            Node resultNode = _firstNode;
+
+            for (int i = 0; i < index; i++)
+            {
+                resultNode = resultNode.Next;
+            }
+
+            return resultNode;
+        }
+
+        private void RemoveNode(Node node)
+        {
+            var previousNode = node.Previous;
+            var nextNode = node.Next;
+
+            if (previousNode == null)
+            {
+                _firstNode = nextNode;
+            }
+            else
+            {
+                previousNode.Next = nextNode;
+            }
+
+            if (nextNode == null)
+            {
+                _lastNode = previousNode;
+            }
+            else
+            {
+                _lastNode.Previous = previousNode;
+            }
+        }
+
+        private struct Enumerator : IEnumerator<T>, IEnumerator
+        {
+            private Node _firstNode;
+            private Node _current;
+            private bool _isFirstStep;
+
+            public Enumerator(Node firstNode)
+            {
+                _firstNode = firstNode;
+                _current = null;
+                _isFirstStep = true;
+            }
+
+            public T Current => _current.Value;
+
+            object IEnumerator.Current => _current.Value;
+
+            public bool MoveNext()
+            {
+                if (_isFirstStep)
+                {
+                    _current = _firstNode;
+                    _isFirstStep = false;
+
+                    return true;
+                }
+
+                if (_current == null || _current.Next == null)
+                {
+                    return false;
+                }
+
+                _current = _current.Next;
+
+                return true;
+            }
+
+            public void Reset()
+            {
+                _isFirstStep = true;
+                _current = null;
+            }
+
+            public void Dispose()
+            { }
+        }
+
+        private class Node
+        {
+            public Node(T value, Node previous, Node next)
+            {
+                Value = value;
+                Previous = previous;
+                Next = next;
+            }
+
+            public T Value { get; set; }
+
+            public Node Previous { get; set; }
+
+            public Node Next { get; set; }
         }
     }
 }
