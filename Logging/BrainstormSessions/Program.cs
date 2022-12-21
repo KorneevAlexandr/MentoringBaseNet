4865325abab19d1d162481c969988ef3fccebc26
KorneevAlexandr <sansay_sa@mail.ru>
2022-12-21 22:35:53 +0300

    Added logging to email

--- Logging/BrainstormSessions/Program.cs
+++ Logging/BrainstormSessions/Program.cs
@@ -12,7 +12,13 @@ namespace BrainstormSessions
         {
             {
                 var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-                Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(config)
+                    .WriteTo.Email(
+                        fromEmail: "app@example.com",
+                        toEmail: "support@example.com",
+                        mailServer: "smtp.example.com")
+                    .CreateLogger();
 
                 try
                 {
