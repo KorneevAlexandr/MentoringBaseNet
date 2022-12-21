b1742339bd7446c85e8db0019ce7ce6ca8135710
KorneevAlexandr <sansay_sa@mail.ru>
2022-12-21 23:01:20 +0300

    Added task 1

---
+++ Serialization/SerializationTasks/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationTasks.Models
+{
+    [Serializable]
+    public class Employee
+    {
+        public string Name { get; set; }
+    }
+}
