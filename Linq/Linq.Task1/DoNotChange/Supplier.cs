5ff55c6aef11362c63403b4e8cc6d715d3e80cab
KorneevAlexandr <sansay_sa@mail.ru>
2023-01-09 21:24:55 +0300

    Initial commit

---
+++ Linq/Linq.Task1/DoNotChange/Supplier.cs
@@ -0,0 +1,10 @@
+﻿namespace Linq.Task1.DoNotChange
+{
+	public class Supplier
+	{
+		public string SupplierName { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string Country { get; set; }
+	}
+}
---
+++ Linq/Linq.Task1/LinqTask.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Linq.Task1.DoNotChange;
+
+namespace Linq.Task1
+{
+    public static class LinqTask
+    {
+        public static IEnumerable<Customer> Linq1(IEnumerable<Customer> customers, decimal limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2(
+            IEnumerable<Customer> customers,
+            IEnumerable<Supplier> suppliers
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2UsingGroup(
+            IEnumerable<Customer> customers,
+            IEnumerable<Supplier> suppliers
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Customer> Linq3(IEnumerable<Customer> customers, decimal limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq4(
+            IEnumerable<Customer> customers
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq5(
+            IEnumerable<Customer> customers
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Customer> Linq6(IEnumerable<Customer> customers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Linq7CategoryGroup> Linq7(IEnumerable<Product> products)
+        {
+            /* example of Linq7result
+
+             category - Beverages
+	            UnitsInStock - 39
+		            price - 18.0000
+		            price - 19.0000
+	            UnitsInStock - 17
+		            price - 18.0000
+		            price - 19.0000
+             */
+
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<(decimal category, IEnumerable<Product> products)> Linq8(
+            IEnumerable<Product> products,
+            decimal cheap,
+            decimal middle,
+            decimal expensive
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<(string city, int averageIncome, int averageIntensity)> Linq9(
+            IEnumerable<Customer> customers
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Linq10(IEnumerable<Supplier> suppliers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
