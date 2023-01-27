d77e8e3fa40295d49d68762ce46baf4ac3e67dc4
aliaksandr.karneyeu <aliaksandr.karneyeu@ticketmaster.co.uk>
2023-01-27 18:52:53 +0300

    Added Kata (Lcd-string) with tests

---
+++ UnitTests/Katas.Library/Kata1.LCD_Digits/LcdDigit.cs
@@ -0,0 +1,21 @@
+﻿namespace Katas.Library.Kata1.LCD_Digits
+{
+	public class LcdDigit
+	{
+		public string FirstRow { get; set; }
+
+		public string SecondRow { get; set; }
+
+		public string ThirdRow { get; set; }
+
+		public static LcdDigit operator +(LcdDigit left, LcdDigit right) =>
+			new LcdDigit
+			{
+				FirstRow  = $"{left.FirstRow} {right.FirstRow}",
+				SecondRow = $"{left.SecondRow} {right.SecondRow}",
+				ThirdRow  = $"{left.ThirdRow} {right.ThirdRow}",
+			};
+
+		public override string ToString() => $"{FirstRow}\n{SecondRow}\n{ThirdRow}";
+	}
+}
