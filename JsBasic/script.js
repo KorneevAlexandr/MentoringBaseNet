a61e5a32d015e52c7161e8a6fb6ca5d829bddd7a
aliaksandr.karneyeu <aliaksandr.karneyeu@ticketmaster.co.uk>
2023-02-05 22:06:07 +0300

    Fix methods

--- JsBasic/script.js
+++ JsBasic/script.js
@@ -1,6 +1,6 @@
 // task 1
 var bananaLine = "I can eat bananas all day";
-alert(bananaLine.split(' ')[3]);
+alert(bananaLine.split(' ')[3].toUpperCase());
 
 // task 2
 var cars = ["Saab", "Volvo", "BMW"];
@@ -9,6 +9,6 @@ var bmw = cars[2]; // get BMW
 cars[0] = "Bentley"; // Change first item
 cars.pop(); // remove last item
 cars[2] = "Audi"; // added new item Audi
-cars[1] += bmw; // splice Volvo and BMW
+cars.splice(2, 0, bmw); // splice Volvo and BMW
 
 alert(cars);
